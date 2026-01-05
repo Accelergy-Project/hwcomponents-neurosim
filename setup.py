aea908d5897302bf68428a87e5da8f8e7f6c4885
tanner-andrulis <andrulis@mit.edu>
2026-01-05 09:38:17 -0500

    setup.py -> pyproject.toml

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.build import build
 import sys
 import subprocess
 import os
-
-
-def readme():
-    """Returns readme contents"""
-    with open("README.md") as f:
-        return f.read()
 
 
 class CustomBuildCommand(build):
@@ -27,39 +21,12 @@ class CustomBuildCommand(build):
         except Exception as e:
             print(f"Unexpected error running 'make build': {e}")
             sys.exit(1)
-        
+
         # Call the parent build command
         super().run()
 
 
 setup(
-    name="hwcomponents-neurosim",
-    version="0.1",
-    description="A package for estimating the energy and area of NeuroSim components",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-    ],
-    keywords="hardware energy estimation analog adc neurosim pim processing-in-memory cim",
-    author="Tanner Andrulis",
-    author_email="andrulis@mit.edu",
-    license="MIT",
-    install_requires=[],
-    python_requires=">=3.12",
-    packages=find_packages(),
-    package_data={
-        "hwcomponents_neurosim": [
-            "cells/*",
-            "NeuroSim/main",
-            "default_config.cfg",
-            "neurosim.model.yaml",
-        ],
-    },
-    include_package_data=True,
-    py_modules=["hwcomponents_neurosim"],
-    entry_points={},
     cmdclass={
         "build": CustomBuildCommand,
     },
