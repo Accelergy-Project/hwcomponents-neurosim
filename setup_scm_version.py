501d7e618b006c9178d4126190b824ca72a43b62
tanner-andrulis <andrulis@mit.edu>
2026-01-05 11:15:43 -0500

    Clean up version tag

--- setup_scm_version.py
+++ setup_scm_version.py
@@ -7,7 +7,7 @@ def post_version(version):
     if version.exact:
         return version.format_with("{tag}").lstrip('v')
 
-    base = str(version.tag).lstrip('v') if version.tag else (guess_next_version(version) or "1.0.0")
+    base = str(version.tag).lstrip('v') if version.tag else (guess_next_version(version) or "1.0")
     distance = version.distance or 0
 
     return f"{base}.{distance}" if distance > 0 else base
